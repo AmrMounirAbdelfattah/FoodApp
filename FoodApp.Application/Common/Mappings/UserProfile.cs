9655c57fedf36dbab5ea9b8c6260137920d46d66
OsamaElmagdoub <osamaelmagdoub508@gmail.com>
2024-10-06 22:34:06 +0300

    User Login

--- FoodApp.Application/Common/Mappings/UserProfile.cs
+++ FoodApp.Application/Common/Mappings/UserProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using FoodApp.Application.Common.DTOs;
 using FoodApp.Application.Common.ViewModels.Users;
 using FoodApp.Application.CQRS.Users.Commands;
 using FoodApp.Domain.Entities;
@@ -12,6 +13,7 @@ namespace FoodApp.Application.Common.Mappings
             CreateMap<RegisterUserViewModel, RegisterUserCommand>();
             CreateMap<RegisterUserCommand, User>();
             CreateMap<ResetPasswordViewModel, ResetPasswordCommand>();
+            CreateMap<User,UserDto>().ReverseMap();
         }
     }
 
