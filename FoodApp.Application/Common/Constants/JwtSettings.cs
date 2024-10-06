6c3dedd37f603c32ec94e30707cb0e2d1485ba70
OsamaElmagdoub <osamaelmagdoub508@gmail.com>
2024-10-06 22:32:53 +0300

    User Authorization

---
+++ FoodApp.Application/Common/Constants/JwtSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodApp.Application.Common.Constants
+{
+    public static class JwtSettings
+    {
+        public const string SecretKey = "ABYREQ$EWIEOUSLHT#@!WPIDTREFRSEE*&^%DHGFDREE";
+        public const string Issuer = "FoodApp";
+        public const string Audience = "FoodApp-Users";
+        public const int DurationInMinutes = 60;
+    }
+}
