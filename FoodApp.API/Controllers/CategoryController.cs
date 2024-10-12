7c80458597370bc8e8d7f84ea651da02a7fbf731
Amr Mounir <Amr Mounir@DESKTOP-VGT8V2P>
2024-10-12 16:06:06 +0300

    Add Recipe and Delete Category

--- FoodApp.API/Controllers/CategoryController.cs
+++ FoodApp.API/Controllers/CategoryController.cs
@@ -22,5 +22,16 @@ namespace FoodApp.API.Controllers
 
             return ResultViewModel<bool>.Sucess(result, "Category added successfully");
         }
+
+        [HttpDelete("{id}")]
+        public async Task<ResultViewModel<bool>> DeleteCategoryAsync(int id)
+        {
+            var result = await _mediator.Send(new DeleteCategoryCommand(id));
+
+            if (!result)
+                return ResultViewModel<bool>.Faliure(ErrorCode.UnKnown, "Failed to delete category or category not found");
+
+            return ResultViewModel<bool>.Sucess(result, "Category deleted successfully");
+        }
     }
 }
--- FoodApp.API/Controllers/RecipeController.cs
+++ FoodApp.API/Controllers/RecipeController.cs
@@ -1,7 +1,9 @@
 ﻿using FoodApp.Application.Common.Helpers;
 using FoodApp.Application.Common.ViewModels;
 using FoodApp.Application.Common.ViewModels.Recipes;
+using FoodApp.Application.CQRS.Recipes.Commands;
 using FoodApp.Application.CQRS.Recipes.Queries;
+using FoodApp.Domain.Enums;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,6 +21,26 @@ namespace FoodApp.API.Controllers
             var recipesVM = result.AsQueryable().Map<RecipeViewModel>().AsEnumerable();
 
             return ResultViewModel<IEnumerable<RecipeViewModel>>.Sucess(recipesVM, "Successfully Get All Recipes");
+        }
+
+        [HttpPost]
+        public async Task<ResultViewModel<bool>> AddRecipeAsync([FromBody] AddRecipeDto recipeDto)
+        {
+            if (recipeDto == null)
+            {
+                return ResultViewModel<bool>.Faliure(ErrorCode.UnKnown,"Invalid recipe data");
+            }
+
+            var result = await _mediator.Send(new AddRecipeCommand(recipeDto));
+
+            if (result)
+            {
+                return ResultViewModel<bool>.Sucess(result, "Recipe added successfully");
+            }
+            else
+            {
+                return ResultViewModel<bool>.Faliure(ErrorCode.EmptyCategoryName,"Failed to add recipe. Category not found.");
+            }
         }
     }
 }
